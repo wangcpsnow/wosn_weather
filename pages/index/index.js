78c6dad47a8e5b725904dbb10653b6891fe603d4
王春鹏 <wangcpsnow@gmail.com>
2017-01-06 18:22:32 +0800

    ajax baidu api

--- pages/index/index.js
+++ pages/index/index.js
@@ -25,10 +25,28 @@ Page({
         that.getLocation();
     },
     getLocation: function() {
+        let self = this;
         wx.getLocation({
             success: (res) => {
-                console.log(res);
+                // console.log(res);
+                let url = `http://api.map.baidu.com/telematics/v3/weather?location=${res.longitude},${res.latitude}&output=json&ak=551220f698647648162cd1b479ef71e2`;
+                wx.request({
+                    url: url,
+                    success: function(data) {
+                        // console.log(data);
+                        self.showWt(data.data);
+                    }
+                });
             }
         })
+    },
+    showWt: function(data) {
+        if(data.status != "success"){
+            wx.showToast({
+                title: "请求天气信息失败,请稍后重试"
+            });
+            return;
+        }
+        let results = data.results[0];
     }
 })
