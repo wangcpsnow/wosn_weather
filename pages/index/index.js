8b00f17ee23e1f883689d0aaa2a8329526ccd937
王春鹏 <wangcpsnow@gmail.com>
2017-01-11 16:31:28 +0800

    m

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,36 +4,22 @@ var app = getApp()
 Page({
     data: {
         motto: 'Hello World',
-        userInfo: {}
-    },
-    //事件处理函数
-    bindViewTap: function() {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
+        userInfo: {},
+        weather: {
+            currentCity: "北京"
+        }
     },
     onLoad: function() {
-        var that = this;
-
-        //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo) {
-            //更新数据
-            that.setData({
-                userInfo: userInfo
-            })
-        })
-        that.getLocation();
+        this.getLocation();
     },
     getLocation: function() {
         let self = this;
         wx.getLocation({
             success: (res) => {
-                // console.log(res);
                 let url = `http://api.map.baidu.com/telematics/v3/weather?location=${res.longitude},${res.latitude}&output=json&ak=551220f698647648162cd1b479ef71e2`;
                 wx.request({
                     url: url,
                     success: function(data) {
-                        // console.log(data);
                         self.showWt(data.data);
                     }
                 });
@@ -48,5 +34,10 @@ Page({
             return;
         }
         let results = data.results[0];
+        console.info(results);
+        this.setData({
+            weather: results,
+            wtinfo: results.weather_data[0]
+        });
     }
 })
